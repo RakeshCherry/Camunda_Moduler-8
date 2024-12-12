d60e757d7a4363d63b0dd73024e11a8ca8c628da
RAKESH M B <rakeshbhavimani1@gmail.com>
2024-12-12 12:58:31 +0530

    first commit

---
+++ src/main/java/com/camunda/project/ReserveSeatOnBoat.java
@@ -0,0 +1,29 @@
+package com.camunda.project;
+
+import jakarta.inject.Named;
+import org.camunda.bpm.engine.delegate.BpmnError;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+@Named
+public class ReserveSeatOnBoat implements JavaDelegate {
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        String money = "0.0";
+        String ticketType = "Coach";
+
+        money = (String) delegateExecution.getVariable("money");
+        double moneyDouble = Double.parseDouble(money);
+
+        if(moneyDouble >= 1000){
+            ticketType = "First Class";
+        }else if(moneyDouble >= 5000){
+            ticketType = "Business Class";
+        }else if(moneyDouble <= 10){
+            ticketType = "Stowaway Class";
+            throw new BpmnError("Full_Overboard", "A Cheap ticket has led to a small wave throwing you overboard.");
+        }
+        delegateExecution.setVariable( "ticketType",ticketType);
+    }
+}
